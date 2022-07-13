2780774409eee1c37005adf2663c5408925614f9
Klaudiuszu <klaudiuszpupiec.kontakt@gmail.com>
2022-07-13 15:29:20 +0200

    map work

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,17 +3,18 @@ import { About, Footer, Header, Skills, Testimonial, Work, MapInfo, TextTramsfor
 import { Navbar } from './components';
 import './App.scss';
 
+
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Testimonial />
-      <MapInfo />
-      <Footer />
+      <Header className='app-scrollSection' />
+      <About className='app-scrollSection' />
+      <Work className='app-scrollSection' />
+      <Skills className='app-scrollSection'/>
+      <Testimonial className='app-scrollSection' />
+      <MapInfo className='app-scrollSection' />
+      <Footer className='app-scrollSection' />
     </div>
   )
 }
