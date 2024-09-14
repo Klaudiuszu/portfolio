5b78891e528b315feb1615b4f99deb7dcff7928e
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2024-09-14 13:30:19 +0200

    update cv

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -48,9 +48,8 @@ const Work = () => {
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text-work ${
-              activeFilter === item ? 'item-active' : ''
-            }`}
+            className={`app__work-filter-item app__flex p-text-work ${activeFilter === item ? 'item-active' : ''
+              }`}
           >
             {item}
           </div>
@@ -72,16 +71,18 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
+                {work.projectLink && (
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                )}
                 {work.codeLink && (
                   <a href={work.codeLink} target="_blank" rel="noreferrer">
                     <motion.div
